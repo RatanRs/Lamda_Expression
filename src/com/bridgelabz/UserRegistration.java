52e88068d7bcb7ac2cdcff725ad28f765b0f0148
9663461085 <ratansollapure16310@gmail.com>
2022-12-27 07:11:00 +0530

    Added:Used  lamda expression for user resistration programm

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.regex.Pattern;
+
 /***
  * 
  * @author Admin
@@ -11,6 +13,47 @@ public class UserRegistration {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Welcome to lamda expression");
-	}
+        UserValidation firstName=(firstname)->{
+            Boolean check = Pattern.matches("^[A-Z][a-z]{2,}$",firstname);
+            if (check == true) {
+                System.out.println("First name is Valid");
+            }else
+                System.out.println("First name is Invalid");
+        };
+        firstName.userValidate("Ratan");
+        UserValidation lastName=(lastname)->{
+            Boolean check = Pattern.matches("^[A-Z][a-z]{2,}$",lastname);
+            if (check == true) {
+                System.out.println("Last name is Valid");
+            }else
+                System.out.println("Last name is Invalid");
+        };
+        lastName.userValidate("Kumar");
+        UserValidation emailId=(email)->{
+            Boolean check = Pattern.matches("^([A-Za-z0-9]{3,}+)((?:[\\.\\+\\-][a-zA-Z0-9]+)*)@([a-z0-9]{1,}+)\\.([a-z]{2,}+)((?:\\.[a-zA-Z]+)?)$",email);
+            if (check == true) {
+                System.out.println("Email Id is Valid");
+            }else
+                System.out.println("Email Id is Invalid");
+        };
+        emailId.userValidate("abc.xyz@bl.co.in");
+        UserValidation mobileNumber=(mobilenumber)->{
+            Boolean check = Pattern.matches("^(91 )[0-9]{10}$",mobilenumber);
+            if (check == true) {
+                System.out.println("Mobile number is Valid");
+            }else
+                System.out.println("Mobile number is Invalid");
+        };
+        mobileNumber.userValidate("91 9999999999");
+        UserValidation passWord=(password)->{
+            Boolean check = Pattern.matches("^(?=.*[A-z])(?=.*[0-9])(?=.*[~`!@#$%^&*_])[a-zA-Z0-9~`!@#$%^&*_]{8,}$",password);
+            if (check == true) {
+                System.out.println("Password is Valid");
+            }else
+                System.out.println("Password is Invalid");
+        };
+        passWord.userValidate("ratanKumar@22225");
+
+    }
+
 }
---
+++ src/com/bridgelabz/UserValidation.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+
+@FunctionalInterface
+public interface UserValidation{
+    void userValidate(String userCredentials);
+}
