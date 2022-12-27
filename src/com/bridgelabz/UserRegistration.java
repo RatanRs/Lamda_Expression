ed4c692e6629e4c15fca9ee6f6c12b62d57b008b
9663461085 <ratansollapure16310@gmail.com>
2022-12-27 07:03:40 +0530

    Added:Welcome meassge to lamda expression programm

---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class UserRegistration {
+	/***
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to lamda expression");
+	}
+}
